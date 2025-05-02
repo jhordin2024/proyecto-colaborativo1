cd8f1cf245f6baa09cc5faa1536055b64abc6cf0
jhordin2024 <1515476@senati.pe>
2025-05-02 16:28:48 -0500

    Add files via upload

---
+++ pagina_prueba DE Amor/Pyton/java.js
@@ -0,0 +1,7 @@
+document.getElementById("yesbtn").addEventListener("click", function() {
+    alert("¡Estoy increíblemente feliz de escuchar eso! Yo también deseo un futuro contigo. Estoy emocionado por la oportunidad de construir una vida juntos, llena de amor y aventuras. Quiero que sepas que siempre estaré aquí para ti, apoyándote en cada paso del camino. Espero que podamos hacer realidad nuestros sueños y metas juntos. Te amo más de lo que las palabras pueden expresar y estoy emocionado por lo que el futuro nos depara. Juntos, podemos enfrentar cualquier desafío y disfrutar de cada momento que compartimos.");
+    
+});
+document.getElementById("nobtn").addEventListener("click", function() {
+    alert("Entiendo y respeto tu decisión &#128148;. Aprecio tu honestidad y el tiempo que compartimos. Aunque es difícil aceptarlo, te deseo sinceramente lo mejor. Siempre valoraré nuestra conexión y los momentos que compartimos juntos. Estoy aquí para ti si alguna vez necesitas apoyo o amistad.");
+});
